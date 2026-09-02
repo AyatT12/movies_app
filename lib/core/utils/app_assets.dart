fb5a932dc5732941118d855f11a4be4a3c274d00
AyatT12 <ayat074@gmail.com>
2026-09-02 03:08:12 +0300

    - adding login screen

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -7,4 +7,8 @@ class AppAssets {
   static const String fourthOnBoarding = '${_base}onboarding4.png';
   static const String fifthOnBoarding = '${_base}onboarding5.png';
   static const String sixthOnBoarding = '${_base}onboarding6.png';
+  static const String logo = '${_base}logo.png';
+  static const String googleIcon = '${_base}google_icon.png';
+  static const String egIcon = '${_base}EG.png';
+  static const String usIcon = '${_base}US.png';
 }
